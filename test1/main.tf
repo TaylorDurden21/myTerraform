41ba32dbc12e2db6651a2a8cbdf29247307bf398
Edward Norton <EdwardNoron@fight.com>
2024-11-23 18:28:13 +0100

    Module du VPC

--- test1/main.tf
+++ test1/main.tf
@@ -2,3 +2,18 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_ssm_parameter" "this" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+module "vpc" {
+  source = "./module_vpc"
+  region = var.region
+}
+
+resource "aws_instance" "my-instance" {
+    ami = data.aws_ssm_parameter.this
+    subnet_id = module.vpc.subnet_id
+    security_groups = module.vpc.security_group_id
+    instance_type = "t3.micro"
+}
---
+++ test1/module_vpc/main.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+    region = var.region
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "this" {
+  vpc_id = aws_vpc.this.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  route = {
+    cidr_block = aws_vpc.this.cidr_block
+    gataway_id = "local"
+  }
+
+  route{
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    name = "MainRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "this" {
+    subnet_id = aws_subnet.this.id
+    route_table_id = aws_route_table.this.id
+}
+
+resource "aws_security_group" "this" {
+  name = "allow_ssh_http_internet"
+  description = "Allow SSH and Http ingress and internet all egress"
+  vpc_id = aws_vpc.this.id
+  
+  tags = {
+    name = "allow_ssh_http_internet_all"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "this" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4 = aws_vpc.this.cidr_block
+  from_port = 80
+  to_port = 80
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "this" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4 = aws_vpc.this.cidr_block
+  from_port = 22
+  to_port = 22
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_engress_rule" "this" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+# VPC - Subnet - Internet Gateway - Route Table - Route table association - SG (allow http & SSH) 
+# S3 Module 
+#alb module
+# Secret Manager module 
+#Module AMI
+#Générer une AMI puis la déployer via un template sur un EC2 
