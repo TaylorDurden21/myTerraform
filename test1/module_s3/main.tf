452d9edaa1ab38155084b39a29a7f8812ab48cb2
bytel <JLUNA@bt0d0000.w2k.bouyguestelecom.fr>
2024-11-26 13:05:14 +0100

    debug S3

--- test1/module_s3/main.tf
+++ test1/module_s3/main.tf
@@ -12,7 +12,7 @@ resource "random_string" "ramdom" {
 
 resource "aws_s3_bucket" "this" {
   count = var.static_web_S3 ? 1 : 0
-  bucket = "tfgenerate-${data.aws_caller_identity.current.account_id}-${random_string.ramdom}"
+  bucket = "tfgenerate-${data.aws_caller_identity.current.account_id}-${random_string.ramdom.result}"
 
   tags = {
     Name  = "My S3 generate by terraform" 
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "this" {
 
 resource "aws_s3_bucket_website_configuration" "this" {
   count = var.static_web_S3 ? 1 : 0
-  bucket = "tfgeneratewebsite-${data.aws_caller_identity.current.account_id}-${random_string.ramdom}"
+  bucket = "tfgeneratewebsite-${data.aws_caller_identity.current.account_id}-${random_string.ramdom.result}"
 
   index_document {
     suffix = var.index_static_web_S3
