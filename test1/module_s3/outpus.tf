dd913ee6434b69a3140606492a3882a192a5eb08
bytel <JLUNA@bt0d0000.w2k.bouyguestelecom.fr>
2024-11-26 13:12:37 +0100

    ajout

--- test1/module_s3/outpus.tf
+++ test1/module_s3/outpus.tf
@@ -1,3 +1,7 @@
 #Génére un bucket S3 et en fonctions des variable d'entrée le met en public ou en privé
 #Peut aussi générer un S3 pour supportter un site en statique
 
+
+output "addrese_DNS_web_static_S3" {
+  value = aws_s3_bucket_website_configuration.this.website_endpoint
+}
--- test1/outputs.tf
+++ test1/outputs.tf
@@ -3,5 +3,5 @@ output "ec2_ID" {
 }
 
 output "addrese_DNS_web_static_S3" {
-  value = module.S3.addrese_DNS_web_static_S3
+  value = module.S3.addrese_DNS_web_static_S3.value
 }
