547ad4ad16900aaa47d7ff75f8bd15b342db0b66
bytel <JLUNA@bt0d0000.w2k.bouyguestelecom.fr>
2024-12-03 11:54:05 +0100

    debug

--- test1/module_s3/main.tf
+++ test1/module_s3/main.tf
@@ -4,14 +4,14 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-resource "random_string" "ramdom" {
+resource "random_string" "random" {
   length = 6
   upper = false
   special = false
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket = "tfgenerate-${data.aws_caller_identity.current.account_id}-${random_string.ramdom.result}"
+  bucket = "tfgenerate-${data.aws_caller_identity.current.account_id}-${random_string.random.result}"
 
   tags = {
     Name  = "My S3 generate by terraform" 
@@ -33,7 +33,35 @@ resource "aws_s3_bucket_website_configuration" "this" {
 
 resource "aws_s3_account_public_access_block" "public_acces_block" {
   count = var.index_static_web_S3 ? 1 : 0
-  block_public_acls = true
-  block_public_policy = true
-  
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+  }
+
+resource "aws_s3_object" "upload_object" {
+  for_each = fileset("web_source/","*")
+  bucket = aws_s3_bucket.this.id
+  key = each.value
+  source = "web_source/${each.value}"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "allow_from_everyone" {
+  bucket = aws_s3_bucket.this.id
+  policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+            "s3:GetObject",
+          ]
+          Effect = "Allow"
+          Resource = "${aws_s3_bucket.this.arn}/*"
+          Principal = "*"
+        }
+      ]
+    }
+  )
 }
--- test1/module_s3/outpus.tf
+++ test1/module_s3/outpus.tf
@@ -1,7 +1,6 @@
 #Génére un bucket S3 et en fonctions des variable d'entrée le met en public ou en privé
 #Peut aussi générer un S3 pour supportter un site en statique
 
-
 output "addrese_DNS_web_static_S3" {
   value = aws_s3_bucket_website_configuration.this[0].website_endpoint
 }
