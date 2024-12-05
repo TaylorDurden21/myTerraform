86041b218798e4caf1a9bdb6751fdade9901c144
bytel <JLUNA@bt0d0000.w2k.bouyguestelecom.fr>
2024-12-05 10:18:22 +0100

    debug22

--- test1/module_s3/outpus.tf
+++ test1/module_s3/outpus.tf
@@ -1,10 +1,4 @@
-#Génére un bucket S3 et en fonctions des variable d'entrée le met en public ou en privé
-#Peut aussi générer un S3 pour supportter un site en statique
-
-output "addrese_DNS_web_static_S3" {
-  value = aws_s3_bucket_website_configuration.this[0].website_endpoint
-}
-
+#id du bucket S3 générer via le module
 output "bucket_id" {
   value = aws_s3_bucket.this.id
 }
