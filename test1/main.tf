47047e79b6fd16047cc5eca0203a54362136dee4
bytel <JLUNA@bt0d0000.w2k.bouyguestelecom.fr>
2024-12-10 11:22:52 +0100

    mise à jour du code

--- test1/main.tf
+++ test1/main.tf
@@ -91,6 +91,7 @@ resource "aws_instance" "my-instance" {
 #Associer le droit sur le ec2 pour qu'il puissé télécharger sur le S3 
 #Télécharger le fichier sur le S3 
 # Le déplacer au bonne endroit sur le serveur web
+#Création d'un module de EC2 site web
   tags = {
     Name  = "My_EC2_generate_by_terraform" 
   }
